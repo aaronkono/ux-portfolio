b0dc374881c93fbeb7f80edf4b05d613e4fe490b
Aaron Konopka <aaron.wakeboardr@gmail.com>
2020-05-24 12:56:32 +0930

    styled components

---
+++ pages/fix-platform-manage/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const SectionContainer = styled.section`
+  display: flex;
+  flex-direction: row;
+  max-width: 1140px;
+  margin: 0 auto;
+`;
+export const LeftColumn = styled.div`
+  width: 50%;
+`;
+export const RightColumn = styled.div`
+  width: 49%;
+`;
+
+export const StickyContainer = styled.div`
+  position: sticky;
+  top: 56px;
+`;
